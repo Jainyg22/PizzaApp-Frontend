35e31a9909a28f6524be10b75ebebeeb9a37d03f
Chirag Jain <jainc451@gmail.com>
2025-12-24 17:54:30 +0530

    Added highlights points

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,9 @@ import IconArrowRight from "../Components/Icons/ArrowRight";
 import PizzaImage from '../assets/Images/pizza2.png';
 import CookingImage from '../assets/Images/cooking1.png';
 import IconPatchCheck from "../Components/Icons/IconPatchCheck";
-
+import OrderFood from '../assets/Images/orderFood.png';
+import Pickup from '../assets/Images/pickup.png';
+import Enjoy from '../assets/Images/enjoy.png';
 function Home() {   
     return (
         <div>
@@ -54,6 +56,7 @@ function Home() {
             </section>
 
 
+            
             {/* Services section */}
             <section
                 className="py-4 mt-6 bg-linear-to-r from-amber-50 to-orange-300"
@@ -99,6 +102,63 @@ function Home() {
                             </div>
                         </div>
                         
+                        <div className="px-5 py-4 mx-auto">
+
+                            <div className="flex justify-center py-4">
+
+                                <div className="inline-flex w-16 h-1 bg-yellow-500 rounded-full"></div>
+
+                            </div>
+
+                            <div className="flex flex-wrap space-y-6 md:space-y-0">
+
+                                <div className="flex flex-col items-center text-center p-4 md:w-1/3">
+                                    <div className="inline-flex items-center justify-center shrink-0 w-20 h-20 mb-5 bg-yellow-100 rounded-full">
+                                        <img src={OrderFood} />
+                                    </div>
+                                    <div className="grow">
+                                        <h2 className="mb-3 text-lg font-bold text-gray-900 title-font">
+                                            Order Food
+                                        </h2>
+                                        <p className="text-base leading-relaxed">
+                                            As easy as 1, 2, 3. Just select your favorite pizza and place your order.
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <div className="flex flex-col items-center text-center p-4 md:w-1/3">
+                                    <div className="inline-flex items-center justify-center shrink-0 w-20 h-20 mb-5 bg-yellow-100 rounded-full">
+                                        <img src={Pickup} />
+                                    </div>
+                                    <div className="grow">
+                                        <h2 className="mb-3 text-lg font-bold text-gray-900 title-font">
+                                            Pickup Food
+                                        </h2>
+                                        <p className="text-base leading-relaxed">
+                                            Pick up your order from the nearest store or get it delivered to your doorstep.
+                                        </p>
+                                    </div>
+
+                                </div>
+                                <div className="flex flex-col items-center text-center p-4 md:w-1/3">
+                                    <div className="inline-flex items-center justify-center shrink-0 w-20 h-20 mb-5 bg-yellow-100 rounded-full">
+                                        <img src={Enjoy} />
+                                    </div>
+                                    <div className="grow">
+                                        <h2 className="mb-3 text-lg font-bold text-gray-900 title-font">
+                                            Enjoy Food
+                                        </h2>
+                                        <p className="text-base leading-relaxed">
+                                            As soon as you get your order, enjoy the delicious pizza with your loved ones.
+                                        </p>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
                     </div>
 
                 </div>
