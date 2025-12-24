e7a1844b0bc62dd3ac93a31db7d6ae1214f39afb
Chirag Jain <jainc451@gmail.com>
2025-12-24 19:43:19 +0530

    Added redux thunks for logout and login

--- src/Layouts/Layout.jsx
+++ src/Layouts/Layout.jsx
@@ -1,8 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Footer from '../Components/Footer';
 import Pizzalogo from '../assets/Images/pizza1.png';
+import { Link } from 'react-router-dom';
+import { logout } from '../Redux/Slices/AuthSlice';
 
 // eslint-disable-next-line react/prop-types
 function Layout({ children }) {
+
+    const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
+
+    async function handleLogout(e) {
+        e.preventDefault();
+        dispatch(logout());
+        
+    }
+
     return (
         <div>
 
@@ -34,6 +47,18 @@ function Layout({ children }) {
                     </ul>
                 </div>
 
+                <div>
+                    <ul className='flex gap-4'>
+                        <li className='hover:text-[#FF9110]'>
+                            {isLoggedIn ? (
+                                <Link onClick={handleLogout}>Logout</Link>
+                            ) : (
+                                <Link to={'/auth/login'}>Login</Link>
+                            )}
+                        </li>
+                    </ul>
+                </div>
+                
             </nav>
 
                 {children}
