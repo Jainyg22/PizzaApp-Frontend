6cf08f13729792f6dc2ecdcd832b1457e4b91d62
Chirag Jain <jainc451@gmail.com>
2025-12-24 17:03:11 +0530

    Added order now button

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <h1 className="text-4xl text-green-600 font-bold">
-      Tailwind OK
-    </h1>
+    <div>
+      <Home />
+    </div>
   )
 }
 
